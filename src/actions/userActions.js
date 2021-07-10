624d1cbf0e961c2c1c2df3dfce86bcc62e714158
Illyas <illyas.deulect@gmail.com>
2021-07-10 13:06:18 +0530

    added registration screen, functionality

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -4,6 +4,9 @@ import {
   USER_LOGIN_REQUEST,
   USER_LOGIN_SUCCESS,
   USER_LOGOUT,
+  USER_REGISTER_FAIL,
+  USER_REGISTER_REQUEST,
+  USER_REGISTER_SUCCESS,
 } from "../constants/userContants";
 
 export const login = (email, password) => async (dispatch) => {
@@ -42,7 +45,49 @@ export const login = (email, password) => async (dispatch) => {
   }
 };
 
-export const logout = (dispatch) => {
+export const logout = () => (dispatch) => {
   localStorage.removeItem("userInfo");
   dispatch({ type: USER_LOGOUT });
+};
+
+export const register = (name, email, password) => async (dispatch) => {
+  try {
+    dispatch({
+      type: USER_REGISTER_REQUEST,
+    });
+    // wen we sending data we have to send header type along with that.
+    // in the case of post here
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+
+    const { data } = await axios.post(
+      "https://ecommerce-rest-backend.herokuapp.com/api/users",
+      { name, email, password },
+      config
+    );
+
+    dispatch({
+      type: USER_REGISTER_SUCCESS,
+      payload: data,
+    });
+    //login to the app at the time of registration
+    //we get same data at both times
+    dispatch({
+      type: USER_LOGIN_SUCCESS,
+      payload: data,
+    });
+
+    localStorage.setItem("userInfo", JSON.stringify(data));
+  } catch (error) {
+    dispatch({
+      type: USER_REGISTER_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
 };
--- src/store.js
+++ src/store.js
@@ -6,13 +6,14 @@ import {
   productDetailsReducer,
   productListReducer,
 } from "./reducers/productReducer";
-import { userLoginReducer } from "./reducers/userReducers";
+import { userLoginReducer, userRegisterReducer } from "./reducers/userReducers";
 
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
   cart: cartReducer,
   userLogin: userLoginReducer,
+  userRegister: userRegisterReducer,
 });
 
 const cartItemFromStorage = localStorage.getItem("cartItems")
