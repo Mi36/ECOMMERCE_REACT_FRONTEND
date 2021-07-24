e1c431b9212fe8847c78c99c702947a49cb25a6c
Illyas <illyas.deulect@gmail.com>
2021-07-24 15:24:48 +0530

    added product edit screen

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ import PlaceOrderScreen from "./screens/PlaceOrderScreen";
 import OrderScreen from "./screens/OrderScreen";
 import UserListScreen from "./screens/UserListScreen";
 import ProductListScreen from "./screens/ProductListScreen";
+import ProductEditScreen from "./screens/ProductEditScreen";
 
 const App = () => {
   return (
@@ -37,6 +38,7 @@ const App = () => {
           <Route path="/product/:id" component={ProductScreen} />
           <Route path="/payment" component={PaymentScreen} />
           <Route path="/placeorder" component={PlaceOrderScreen} />
+          <Route path="/admin/product/:id/edit" component={ProductEditScreen} />
           {/* ? added to make id optional */}
           <Route path="/cart/:id?" component={CartScreen} />
           <Route path="/" component={HomeScreen} exact />
