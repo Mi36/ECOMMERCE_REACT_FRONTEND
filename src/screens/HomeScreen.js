742a9bfe205e63c06b75690560fc118c1518b825
Illyas <illyas.deulect@gmail.com>
2021-08-21 01:52:07 +0530

    ui changes in homescreen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,12 +1,9 @@
 import React, { useEffect } from "react";
-import { Col, Row } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { listProducts } from "../actions/productActions";
 import Meta from "../components/Meta";
-import Paginate from "../components/Paginate";
-import { Product } from "../components/Product";
-import ProductCarousel from "../components/ProductCarousel";
+import styles from "../styles/homeScreen.module.css";
 
 const HomeScreen = ({ match }) => {
   const dispatch = useDispatch();
@@ -19,17 +16,15 @@ const HomeScreen = ({ match }) => {
   }, [dispatch, keyword, pageNumber]);
 
   const productList = useSelector((state) => state.productList);
-  const { loading, error, products, page, pages } = productList;
+  const { loading, error, products } = productList;
   return (
     <>
       <Meta />
-      {!keyword ? (
-        <ProductCarousel />
-      ) : (
-        <Link to="/" className="btn btn-li">
-          GO BACK
-        </Link>
-      )}
+
+      <Link to="/" className="btn btn-li">
+        GO BACK
+      </Link>
+
       <h1>Latest products</h1>
       {loading ? (
         <h2>Loading.....</h2>
@@ -37,17 +32,86 @@ const HomeScreen = ({ match }) => {
         <h3>{error}</h3>
       ) : (
         <>
-          <Row>
-            {products?.length > 0 &&
-              products.map((product) => {
-                return (
-                  <Col key={product._id}>
-                    <Product product={product} />
-                  </Col>
-                );
-              })}
-          </Row>
-          <Paginate pages={pages} page={page} />
+          {products?.length > 0 &&
+            products.map((product) => {
+              return (
+                <div className={styles.header}>
+                  <div>
+                    <div>
+                      <img src={product.image} alt="product" />
+                    </div>
+                  </div>
+                  <div>
+                    <div>
+                      <h2>{product.category}</h2>
+                      <div>
+                        <span>
+                          <i
+                            className={
+                              product?.rating >= 1
+                                ? "fas fa-star"
+                                : product?.rating >= 0.5
+                                ? "fas fa-star-half-alt"
+                                : "far fa-star"
+                            }
+                          ></i>
+                        </span>
+                        <span>
+                          <i
+                            className={
+                              product?.rating >= 2
+                                ? "fas fa-star"
+                                : product?.rating >= 1.5
+                                ? "fas fa-star-half-alt"
+                                : "far fa-star"
+                            }
+                          ></i>
+                        </span>
+                        <span>
+                          <i
+                            className={
+                              product?.rating >= 3
+                                ? "fas fa-star"
+                                : product?.rating >= 2.5
+                                ? "fas fa-star-half-alt"
+                                : "far fa-star"
+                            }
+                          ></i>
+                        </span>
+                        <span>
+                          <i
+                            className={
+                              product?.rating >= 4
+                                ? "fas fa-star"
+                                : product?.rating >= 3.5
+                                ? "fas fa-star-half-alt"
+                                : "far fa-star"
+                            }
+                          ></i>
+                        </span>
+                        <span>
+                          <i
+                            className={
+                              product?.rating >= 5
+                                ? "fas fa-star"
+                                : product?.rating >= 4.5
+                                ? "fas fa-star-half-alt"
+                                : "far fa-star"
+                            }
+                          ></i>
+                        </span>
+                        <span>
+                          {` `}
+                          {product?.numReviews} Reviews
+                        </span>
+                      </div>
+                    </div>
+                    <Link to={`/product/${product._id}`}>{product.name}</Link>
+                    <p>{product.price}</p>
+                  </div>
+                </div>
+              );
+            })}
         </>
       )}
     </>
