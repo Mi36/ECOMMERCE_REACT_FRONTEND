22270734a6b69f6995c54d4dc5bdc2851f9958cd
Illyas <illyas.deulect@gmail.com>
2021-08-21 13:57:27 +0530

    ui fixes in product screen

--- src/screens/ProductScreen.js
+++ src/screens/ProductScreen.js
@@ -1,18 +1,10 @@
 import React, { useEffect, useState } from "react";
-import {
-  Button,
-  Card,
-  Col,
-  Form,
-  Image,
-  ListGroup,
-  Row,
-} from "react-bootstrap";
+import { Form } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import {
-  listProductsDetails,
   createProductReview,
+  listProductsDetails,
 } from "../actions/productActions";
 import Loader from "../components/Loader";
 import Message from "../components/Message";
@@ -71,8 +63,9 @@ const ProductScreen = ({ history, match }) => {
     dispatch(listProductsDetails(match.params.id));
   }, [match, dispatch, successProductReview]);
   return (
+    //my-3 means-> marginvertical here
     <>
-      <Link className="btn btn-light my-3" to="/">
+      <Link className="btn btn-dark my-3" to="/">
         Go Back
       </Link>
       {loading ? (
@@ -82,100 +75,87 @@ const ProductScreen = ({ history, match }) => {
       ) : (
         <>
           <Meta title={product.name} />
-          <Row>
-            <Col md={6}>
-              <Image src={product.image} alt={product.name} fluid />
-            </Col>
-            <Col md={3}>
-              <ListGroup variant="flush">
-                <ListGroup.Item>
-                  <h3>{product.name}</h3>
-                </ListGroup.Item>
-                <ListGroup.Item>
-                  <Rating
-                    value={product.rating}
-                    text={`${product.numReviews} reviews`}
-                  />
-                </ListGroup.Item>
-                <ListGroup.Item>Price: ${product.price}</ListGroup.Item>
-                <ListGroup.Item>
-                  Description: {product.description}
-                </ListGroup.Item>
-              </ListGroup>
-            </Col>
-            <Col md={3}>
-              <Card>
-                <ListGroup variant="flush">
-                  <ListGroup.Item>
-                    <Row>
-                      <Col>Price:</Col>
-                      <Col>
-                        <strong>${product.price}</strong>
-                      </Col>
-                    </Row>
-                  </ListGroup.Item>
+          <div>
+            <img src={product.image} alt={product.name} />
 
-                  <ListGroup.Item>
-                    <Row>
-                      <Col>Status:</Col>
-                      <Col>
+            <div>
+              <div>
+                <div>
+                  <h3>{product.name}</h3>
+                </div>
+
+                <Rating
+                  value={product.rating}
+                  text={`${product.numReviews} reviews`}
+                />
+
+                <div>Price: ${product.price}</div>
+                <div>Description: {product.description}</div>
+              </div>
+            </div>
+            <div>
+              <div>
+                <div>
+                  <div>
+                    <div>
+                      <div>Price:</div>
+                      <strong>${product.price}</strong>
+                    </div>
+                  </div>
+
+                  <div>
+                    <div>
+                      <div>Status:</div>
+                      <div>
                         {product.countInStock > 0 ? "In Stock" : "Out Of Stock"}
-                      </Col>
-                    </Row>
-                  </ListGroup.Item>
+                      </div>
+                    </div>
+                  </div>
 
                   {product.countInStock > 0 && (
-                    <ListGroup.Item>
-                      <Row>
-                        <Col>Qty</Col>
-                        <Col>
-                          <Form.Control
-                            as="select"
-                            value={qty}
-                            onChange={(e) => setQty(e.target.value)}
-                          >
-                            {[...Array(product.countInStock).keys()].map(
-                              (x) => (
-                                <option key={x + 1} value={x + 1}>
-                                  {x + 1}
-                                </option>
-                              )
-                            )}
-                          </Form.Control>
-                        </Col>
-                      </Row>
-                    </ListGroup.Item>
+                    <div>
+                      <div>Quantity</div>
+                      <Form.Control
+                        as="select"
+                        value={qty}
+                        onChange={(e) => setQty(e.target.value)}
+                      >
+                        {[...Array(product.countInStock).keys()].map((x) => (
+                          <option key={x + 1} value={x + 1}>
+                            {x + 1}
+                          </option>
+                        ))}
+                      </Form.Control>
+                    </div>
                   )}
 
-                  <ListGroup.Item>
-                    <Button
-                      onClick={addToCartHandler}
-                      className="btn-block"
-                      type="button"
-                      disabled={product.countInStock === 0}
-                    >
-                      Add To Cart
-                    </Button>
-                  </ListGroup.Item>
-                </ListGroup>
-              </Card>
-            </Col>
-          </Row>
-          <Row>
-            <Col md={6}>
+                  <button
+                    onClick={addToCartHandler}
+                    className="btn-block"
+                    type="button"
+                    disabled={product.countInStock === 0}
+                  >
+                    Add To Cart
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div>
+            <div>
               <h2>Reviews</h2>
               {product.reviews.length === 0 && <Message>No Reviews</Message>}
-              <ListGroup variant="flush">
+              <div>
                 {product.reviews.map((review) => (
-                  <ListGroup.Item key={review._id}>
+                  <div key={review._id}>
                     <strong>{review.name}</strong>
                     <Rating value={review.rating} />
                     <p>{review.createdAt.substring(0, 10)}</p>
                     <p>{review.comment}</p>
-                  </ListGroup.Item>
+                  </div>
                 ))}
-                <ListGroup.Item>
-                  <h2>Write a Customer Review</h2>
+                <div>
+                  <h2>Write a Review</h2>
                   {successProductReview && (
                     <Message variant="success">
                       Review submitted successfully
@@ -211,23 +191,23 @@ const ProductScreen = ({ history, match }) => {
                           onChange={(e) => setComment(e.target.value)}
                         ></Form.Control>
                       </Form.Group>
-                      <Button
+                      <button
                         disabled={loadingProductReview}
                         type="submit"
                         variant="primary"
                       >
                         Submit
-                      </Button>
+                      </button>
                     </Form>
                   ) : (
                     <Message>
                       Please <Link to="/login">sign in</Link> to write a review{" "}
                     </Message>
                   )}
-                </ListGroup.Item>
-              </ListGroup>
-            </Col>
-          </Row>
+                </div>
+              </div>
+            </div>
+          </div>
         </>
       )}
     </>
