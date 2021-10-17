59d85f2cd9e562137e758000169cadaefea1ff46
Illyas <illyas.deulect@gmail.com>
2021-10-17 15:31:15 +0530

    homescreen ui updated

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -17,12 +17,40 @@ const HomeScreen = ({ match }) => {
   const productList = useSelector((state) => state.productList);
   const { loading, error, products } = productList;
   return (
-    <div>
+    <div style={{}}>
       <Meta />
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+        }}
+      >
+        <Link
+          to="/"
+          style={{
+            textDecoration: "none",
+            backgroundColor: "brown",
+            borderRadius: 10,
+            paddingLeft: 5,
+            paddingRight: 5,
+            alignSelf: "center",
+            color: "white",
+          }}
+        >
+          GO BACK
+        </Link>
+        <h1
+          style={{
+            textAlign: "center",
+            alignSelf: "center",
+            paddingTop: 10,
+            paddingLeft: 100,
+          }}
+        >
+          Latest products
+        </h1>
+      </div>
 
-      <Link to="/">GO BACK</Link>
-
-      <h1>Latest products</h1>
       {loading ? (
         <h2>Loading.....</h2>
       ) : error ? (
@@ -30,92 +58,87 @@ const HomeScreen = ({ match }) => {
       ) : (
         <div
           style={{
-            marginTop: 120,
+            marginTop: 30,
             marginBottom: 120,
           }}
         >
           {products?.length > 0 &&
             products.map((product) => {
               return (
-                <div>
+                <div style={{}}>
+                  <img
+                    style={{}}
+                    src={product.image}
+                    alt="product"
+                    height="440px"
+                    width="440px"
+                  />
+
                   <div>
-                    <div>
-                      <img
-                        src={product.image}
-                        alt="product"
-                        height="440px"
-                        width="440px"
-                      />
-                    </div>
+                    <h2>{product.category}</h2>
+
+                    <span>
+                      <i
+                        className={
+                          product?.rating >= 1
+                            ? "fas fa-star"
+                            : product?.rating >= 0.5
+                            ? "fas fa-star-half-alt"
+                            : "far fa-star"
+                        }
+                      ></i>
+                    </span>
+                    <span>
+                      <i
+                        className={
+                          product?.rating >= 2
+                            ? "fas fa-star"
+                            : product?.rating >= 1.5
+                            ? "fas fa-star-half-alt"
+                            : "far fa-star"
+                        }
+                      ></i>
+                    </span>
+                    <span>
+                      <i
+                        className={
+                          product?.rating >= 3
+                            ? "fas fa-star"
+                            : product?.rating >= 2.5
+                            ? "fas fa-star-half-alt"
+                            : "far fa-star"
+                        }
+                      ></i>
+                    </span>
+                    <span>
+                      <i
+                        className={
+                          product?.rating >= 4
+                            ? "fas fa-star"
+                            : product?.rating >= 3.5
+                            ? "fas fa-star-half-alt"
+                            : "far fa-star"
+                        }
+                      ></i>
+                    </span>
+                    <span>
+                      <i
+                        className={
+                          product?.rating >= 5
+                            ? "fas fa-star"
+                            : product?.rating >= 4.5
+                            ? "fas fa-star-half-alt"
+                            : "far fa-star"
+                        }
+                      ></i>
+                    </span>
+                    <span>
+                      {` `}
+                      {product?.numReviews} Reviews
+                    </span>
                   </div>
-                  <div>
-                    <div>
-                      <h2>{product.category}</h2>
-                      <div>
-                        <span>
-                          <i
-                            className={
-                              product?.rating >= 1
-                                ? "fas fa-star"
-                                : product?.rating >= 0.5
-                                ? "fas fa-star-half-alt"
-                                : "far fa-star"
-                            }
-                          ></i>
-                        </span>
-                        <span>
-                          <i
-                            className={
-                              product?.rating >= 2
-                                ? "fas fa-star"
-                                : product?.rating >= 1.5
-                                ? "fas fa-star-half-alt"
-                                : "far fa-star"
-                            }
-                          ></i>
-                        </span>
-                        <span>
-                          <i
-                            className={
-                              product?.rating >= 3
-                                ? "fas fa-star"
-                                : product?.rating >= 2.5
-                                ? "fas fa-star-half-alt"
-                                : "far fa-star"
-                            }
-                          ></i>
-                        </span>
-                        <span>
-                          <i
-                            className={
-                              product?.rating >= 4
-                                ? "fas fa-star"
-                                : product?.rating >= 3.5
-                                ? "fas fa-star-half-alt"
-                                : "far fa-star"
-                            }
-                          ></i>
-                        </span>
-                        <span>
-                          <i
-                            className={
-                              product?.rating >= 5
-                                ? "fas fa-star"
-                                : product?.rating >= 4.5
-                                ? "fas fa-star-half-alt"
-                                : "far fa-star"
-                            }
-                          ></i>
-                        </span>
-                        <span>
-                          {` `}
-                          {product?.numReviews} Reviews
-                        </span>
-                      </div>
-                    </div>
-                    <Link to={`/product/${product._id}`}>{product.name}</Link>
-                    <p>{product.price}</p>
-                  </div>
+                  <Link to={`/product/${product._id}`}>{product.name}</Link>
+                  <p>{product.price}</p>
                 </div>
               );
             })}
