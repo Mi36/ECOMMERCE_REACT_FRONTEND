2b62c55a40b38ad19d6c9b3f98b21d59f4d9b081
Illyas <illyas.deulect@gmail.com>
2021-10-19 00:10:39 +0530

    WIP: setup paypal payment

--- src/screens/OrderScreen.js
+++ src/screens/OrderScreen.js
@@ -59,9 +59,8 @@ const OrderScreen = ({ match, history }) => {
       script.type = "text/javascript";
       script.src = `https://www.paypal.com/sdk/js?client-id=${clientId}`;
       script.async = true;
-      script.onload = () => {
-        setSdkReady(true);
-      };
+      script.onload = () => setSdkReady(true);
+
       document.body.appendChild(script);
     };
 
